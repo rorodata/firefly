6f199213a35bf87d17c594c023bf6ed4360f70a0
Nabarun Pal <palnabarun@users.noreply.github.com>
2017-06-15 12:11:06 +0530

    Added support for specifying multiple functions from cmdline
    Fixes Issue #10 (#12)
    
    * Modifies firefly/main.py
    
    * Adds test for load_function

--- firefly/main.py
+++ firefly/main.py
@@ -7,7 +7,7 @@ from .server import FireflyServer
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument("-b", "--bind", dest="ADDRESS", default="127.0.0.1:8000")
-    p.add_argument("function", help="function to serve")
+    p.add_argument("functions", nargs='+', help="functions to serve")
     return p.parse_args()
 
 def load_function(function_spec):
@@ -17,7 +17,14 @@ def load_function(function_spec):
     mod_name, func_name = function_spec.rsplit(".", 1)
     mod = importlib.import_module(mod_name)
     func = getattr(mod, func_name)
-    return func
+    return (func_name, func)
+
+def load_functions(function_specs):
+    return [load_function(function_spec) for function_spec in function_specs]
+
+def add_routes(app, functions):
+    for name, function in functions:
+        app.add_route('/'+name, function)
 
 def main():
     # ensure current directory is added to sys.path
@@ -25,10 +32,10 @@ def main():
         sys.path.insert(0, "")
 
     args = parse_args()
-    function = load_function(args.function)
+    functions = load_functions(args.functions)
 
     app = Firefly()
-    app.add_route("/", function)
+    add_routes(app, functions)
 
     server = FireflyServer(app, {"bind": args.ADDRESS})
     server.run()
