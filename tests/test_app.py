bac672084b22444745d7b288faf3a2c9c96a9e8a
Nabarun Pal <pal.nabarun95@gmail.com>
2017-06-15 14:33:15 +0530

    Adds test case for help text generator function inside Firefly

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,26 @@
 from webob import Request, Response
-from firefly.app import FireflyFunction
+from firefly.app import Firefly, FireflyFunction
 
 def square(a):
+    '''Computes square'''
     return a**2
+
+class TestFirefly:
+    def test_generate_function_list(self):
+        firefly = Firefly()
+        firefly.add_route("/square", square, "square")
+        returned_dict = {
+                "square": {
+                    "path": "/square",
+                    "doc": "Computes square"
+                }
+            }
+        assert firefly.generate_function_list() == returned_dict
 
 class TestFireflyFunction:
     def test_call(self):
         func = FireflyFunction(square)
-        request = Request.blank("/", POST='{"a": 3}')
+        request = Request.blank("/square", POST='{"a": 3}')
         response = func(request)
         assert response.status == '200 OK'
         assert response.text == '9'
