395e4a8783a69f78811b2481004be394ee61dc62
Nabarun Pal <pal.nabarun95@gmail.com>
2017-06-16 23:15:17 +0530

    Modifies test to accomodate for changes in main/load_function()

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,6 +3,7 @@ from firefly.main import load_function
 
 def test_load_functions():
     os.path.exists2 = os.path.exists
-    name, func = load_function("os.path.exists2")
+    path, name, func = load_function("os.path.exists2")
+    assert path == "/exists2"
     assert name == "exists2"
     assert func == os.path.exists
