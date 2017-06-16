8c98dd19d1e2e0216dec4741f16bbf18ba7efe8d
Nabarun Pal <pal.nabarun95@gmail.com>
2017-06-16 12:27:29 +0530

    Adds tests for firefly client

---
+++ tests/test_client.py
@@ -0,0 +1,48 @@
+import pytest
+import requests
+from firefly.client import Client, RemoteFunction, FireflyError
+from firefly.validator import ValidationError
+
+class MockResponse:
+    def __init__(self, status_code, data):
+        self.status_code = status_code
+        self.data = data
+
+    def json(self):
+        return self.data
+
+def make_monkey_patch(status, data):
+    def mock_post_response(url, json):
+        r = MockResponse(status_code=status, data=data)
+        return r
+    return mock_post_response
+
+class TestClass:
+    def test_call_for_success_event(self, monkeypatch):
+        monkeypatch.setattr(requests, "post", make_monkey_patch(200, 16))
+        c = Client("http://127.0.0.1:8000")
+        assert c.square(a=4) == 16
+
+    def test_call_for_validation_error(self, monkeypatch):
+        monkeypatch.setattr(requests, "post", make_monkey_patch(404, {"status": "not found"}))
+        c = Client("http://127.0.0.1:8000")
+        with pytest.raises(FireflyError, message="Expected FireflyError"):
+            c.sq(a=4)
+
+    def test_call_for_validation_error(self, monkeypatch):
+        monkeypatch.setattr(requests, "post", make_monkey_patch(422, {"error": "missing a required argument: 'a'"}))
+        c = Client("http://127.0.0.1:8000")
+        with pytest.raises(ValidationError, message="Expected ValidationError"):
+            c.square(b=4)
+
+    def test_call_for_server_error(self, monkeypatch):
+        monkeypatch.setattr(requests, "post", make_monkey_patch(500, ""))
+        c = Client("http://127.0.0.1:8000")
+        with pytest.raises(FireflyError, message="Expected FireflyError"):
+            c.square(a=4)
+
+    def test_call_for_uncaught_exception(self, monkeypatch):
+        monkeypatch.setattr(requests, "post", make_monkey_patch(502, ""))
+        c = Client("http://127.0.0.1:8000")
+        with pytest.raises(FireflyError, message="Expected FireflyError"):
+            c.square(a=4)
