ab60219395185ef06971a6690b425ea3c584c0ee
Nabarun Pal <pal.nabarun95@gmail.com>
2017-06-23 12:04:49 +0530

    Updates setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 import os.path
+import sys
+
+PY2 = (sys.version_info.major == 2)
 
 def get_version():
     """Returns the package version taken from version.py.
@@ -12,6 +15,16 @@ def get_version():
         exec(code, env, env)
         return env['__version__']
 
+install_requires = [
+    'gunicorn==19.7.1',
+    'WebOb==1.7.2',
+    'requests==2.18.1',
+    'PyYAML==3.12'
+]
+
+if PY2:
+    install_requires.append('funcsigs==1.0.2')
+
 __version__ = get_version()
 
 setup(
@@ -21,10 +34,7 @@ setup(
     author_email='rorodata.team@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'WebOb==1.7.2',
-        'gunicorn==19.7.1'
-    ],
+    install_requires=install_requires,
     entry_points='''
         [console_scripts]
         firefly=firefly.main:main
