07b76e54cc38c0452dce6dc7faa3c4bda067e20b
Anand Chitipothu <anandology@gmail.com>
2018-12-18 17:25:03 +0530

    Added Authorization to Access-Control-Allow-Headers.

--- firefly/app.py
+++ firefly/app.py
@@ -102,7 +102,7 @@ class Firefly(object):
             headers = {
                 'Access-Control-Allow-Origin': self.allowed_origins,
                 'Access-Control-Allow-Methods': 'GET,POST',
-                'Access-Control-Allow-Headers': 'Content-Type'
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization'
             }
             return list(headers.items())
         else:
